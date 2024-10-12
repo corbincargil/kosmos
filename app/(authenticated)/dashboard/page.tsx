c4c14ca26746831b0c3468be4d973386b08b6d9d
Corbin <corbin.cargil@gmail.com>
2024-10-12 16:23:38 -0500

    remove unused imports

--- app/(authenticated)/dashboard/page.tsx
+++ app/(authenticated)/dashboard/page.tsx
@@ -1,47 +1,15 @@
 "use client";
 
 import { useUser } from "@clerk/nextjs";
-import { useEffect, useState } from "react";
 import {
   Card,
   CardHeader,
   CardTitle,
   CardDescription,
-  CardContent,
 } from "@/components/ui/card";
-import { User } from "@/types/user";
-
-interface UserCardProps {
-  email: string;
-  clerkUserId: string;
-}
-
-function UserCard({ email, clerkUserId }: UserCardProps) {
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle>{email}</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <p>Clerk User ID: {clerkUserId}</p>
-      </CardContent>
-    </Card>
-  );
-}
 
 export default function Dashboard() {
   const { user } = useUser();
-  const [users, setUsers] = useState<User[]>([]);
-
-  useEffect(() => {
-    async function fetchUsers() {
-      const response = await fetch("/api/users");
-      const data = await response.json();
-      setUsers(data);
-    }
-
-    fetchUsers();
-  }, []);
 
   return (
     <>
