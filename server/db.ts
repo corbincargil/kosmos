ec407031d376d2634cddea7f8fb5f68921a372de
Corbin <corbin.cargil@gmail.com>
2025-05-19 22:16:01 -0500

    add deploy-preview workflow

--- server/db.ts
+++ server/db.ts
@@ -1,12 +1,7 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaNeon } from '@prisma/adapter-neon'
-
-const connectionString = `${process.env.DATABASE_URL}`
-const adapter = new PrismaNeon({connectionString})
 
 const createPrismaClient = () => {
   return new PrismaClient({
-    adapter,
     log:
       process.env.NODE_ENV === "development"
         ? ["query", "info", "warn", "error"]
